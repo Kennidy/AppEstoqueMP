d3be84ba37cf2c62e7a0ccbedb7302b45705c6dc
Kennidy <kennidypolidoro@gmail.com>
2023-08-14 14:50:01 -0300

    20230807_1708 Inicio de programacao em java

---
+++ app/src/main/java/br/dev/kgb/mobile/estoquemp/model/Produto.java
@@ -0,0 +1,9 @@
+package br.dev.kgb.mobile.estoquemp.model;
+
+public class Produto {
+    //Atributos
+    int categoriaProduto;
+    String nomeProduto;
+    String codigoProduto;
+    int estoqueProduto;
+}
