75db506ba87b9ff75a1ad7a740ae0fe3e795c21d
Kennidy <kennidypolidoro@gmail.com>
2023-08-28 14:26:33 -0300

    20230828 1426 Feito antes do banco de dados

--- app/src/main/java/br/dev/kgb/mobile/estoquemp/ui/produto/CadProdutoFragment.java
+++ app/src/main/java/br/dev/kgb/mobile/estoquemp/ui/produto/CadProdutoFragment.java
@@ -1,5 +1,6 @@
 package br.dev.kgb.mobile.estoquemp.ui.produto;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import br.dev.kgb.mobile.estoquemp.R;
 import br.dev.kgb.mobile.estoquemp.model.Produto;
@@ -40,6 +42,7 @@ public class CadProdutoFragment extends Fragment implements View.OnClickListener
         this.etEstoqueDoProduto = (EditText) view.findViewById(R.id.etEstoque);
         this.spCategoriaDoProduto = (Spinner) view.findViewById(R.id.spCategoria);
         this.btSalvar = (Button) view.findViewById(R.id.btSalvarTela);
+        this.btSalvar.setOnClickListener(this);//definindo o listener do botão
 
         return this.view;
     }
@@ -52,11 +55,17 @@ public class CadProdutoFragment extends Fragment implements View.OnClickListener
               Produto produto = new Produto();
               produto.setNomeProduto(this.etNomeDoProduto.getText().toString());
               produto.setCodigoProduto(this.etCodigoDoProduto.getText().toString());
-
               String qtd = this.etEstoqueDoProduto.getText().toString();  //aux conversão string para inteiro
               produto.setEstoqueProduto(Integer.valueOf(qtd));           //conversão string para inteiro
-
               produto.setCategoriaProduto(this.spCategoriaDoProduto.getSelectedItemPosition());
+
+              //mensagem de sucesso
+              Context context = view.getContext();
+              CharSequence text = "salvo com sucesso!";
+              int duration = Toast.LENGTH_SHORT;
+              Toast toast = Toast.makeText(context, text, duration);
+              toast.show();
+
           }
     }
 }
