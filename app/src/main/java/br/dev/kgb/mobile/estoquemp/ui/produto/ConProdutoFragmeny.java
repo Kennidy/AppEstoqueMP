84220dc4390703c13709095e4639061b8426ba41
Kennidy <kennidypolidoro@gmail.com>
2023-10-30 15:16:54 -0300

    202310230_1516 Quase pronto tela de pesquisa

--- app/src/main/java/br/dev/kgb/mobile/estoquemp/ui/produto/ConProdutoFragmeny.java
+++ app/src/main/java/br/dev/kgb/mobile/estoquemp/ui/produto/ConProdutoFragmeny.java
@@ -12,11 +12,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -27,7 +33,7 @@ import br.dev.kgb.mobile.estoquemp.model.Produto;
  * A fragment representing a list of Items.
  */
 public class ConProdutoFragmeny extends Fragment implements
-        Response.ErrorListener, Response.Listener{
+        Response.ErrorListener, Response.Listener {
 
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
@@ -77,27 +83,76 @@ public class ConProdutoFragmeny extends Fragment implements
         //inicializando a fila de requests do SO
         this.requestQueue.start();
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new ConProdutoRecyclerViewAdapter(produtos));
-        }
+        //array parametro de envio para o serviço
+        JSONArray jsonArray = new JSONArray();
+
+        //objeto com informações de filtro da consulta
+        Produto produto = new Produto();
+        produto.setCodigoProduto("7894900011609");
+
+
+        //incluindo objeto no array de envio
+        jsonArray.put(produto.toJsonObject());
+        //requisição para o Rest Server
+        jsonArrayReq = new JsonArrayRequest(Request.Method.GET,
+                "http://10.0.2.2/conproduto.php",
+                jsonArray, this, this);
+        //mando executar a requisção na fila do sistema
+        requestQueue.add(jsonArrayReq);
+
         return this.view;
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-
+        //mostrar mensagem que veio do servidor
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar a consulta: " +
+                        error.toString(), Snackbar.LENGTH_LONG);
+        mensagem.show();
     }
 
     @Override
     public void onResponse(Object response) {
+
+        try {
+            //array Json para receber a resposta do webservice
+            JSONArray jsonArray = null;
+            jsonArray = new JSONArray(response.toString());
+            //se a consulta não veio vazia passar para array list
+            if (jsonArray != null) {
+                //objeto java
+                Produto produto = null;
+                //array list para receber a resposta
+                this.produtos = new ArrayList<Produto>();
+                //preenchendo ArrayList com JSONArray recebido
+                for (int i = 0, size = jsonArray.length();
+                     i < size; i++) {
+                    JSONObject jo = jsonArray.getJSONObject(i);
+                    produto = new Produto(jo);
+                    this.produtos.add(produto);
+                }
+                // Set the adapter
+                if (view instanceof RecyclerView) {
+                    Context context = view.getContext();
+                    RecyclerView recyclerView = (RecyclerView) view;
+                    if (mColumnCount <= 1) {
+                        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                    } else {
+                        recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+                    }
+                    recyclerView.setAdapter(new ConProdutoRecyclerViewAdapter(produtos));
+                }
+
+            } else {
+                Snackbar mensagem = Snackbar.make(view,
+                        "A consulta não retornou nenhum registro!",
+                        Snackbar.LENGTH_LONG);
+                mensagem.show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 }
